92459a568df451a45c2212dd0a637425354dd3db
Darshan Shah <ds62730@gmail.com>
2025-09-07 13:54:39 +0530

    add toast component

---
+++ src/Components/Toast/Toast.component.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState, useContext } from "react";
+
+import styles from "./Toast.module.css";
+import { ToastContext } from "../../Context/ToastContext";
+
+const Toast = () => {
+  const { toast, setToast } = useContext(ToastContext);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast(null);
+    }, 3000);
+    return () => clearTimeout(timer);
+  });
+
+  if (toast === null) return null;
+
+  return (
+    <div
+      className={`${styles.container} toast-container`}
+      key={new Date().getTime()}
+    >
+      <div
+        className={`${styles.toast}  ${styles[`toast${toast.type}`]}`}
+        role="alert"
+      >
+        <span>{toast.message}</span>
+        <button
+          className={`${styles.toastClose}`}
+          onClick={() => {
+            setToast(null);
+          }}
+        >
+          &times;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
