27c65a71450d047f887e5ee5ec3f755070ee2d53
Darshan Shah <ds62730@gmail.com>
2025-10-18 15:46:05 +0530

    intialize base url based on the device

--- src/Data/server.js
+++ src/Data/server.js
@@ -1,3 +1,10 @@
-export default {
-  baseUrl: "http://localhost:3000",
-};
+const baseUrl = (() => {
+  if (window?.electronAPI?.getSystemIPAddress !== undefined) {
+    return "http://localhost:3000";
+  } else {
+    const currentUR = new URL(window.location.href);
+    return `http://${currentUR.hostname}:3000`;
+  }
+})();
+
+export default { baseUrl: baseUrl };
